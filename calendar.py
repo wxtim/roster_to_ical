67a13ffd9e3e25d0b2f7b4baf6089598807a0e7d
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2017-08-14 20:49:04 +0100

    Small changes to improve pylint score

--- calendar.py
+++ calendar.py
@@ -1,36 +1,14 @@
 '''
 This script was written by Tim Pillinger\r\n
-It's designed to take Leeming shifts\r\n
+It's designed to take shifts\r\n
 and return an ICS diary.\r\n
 '''
 import hashlib
 import datetime as dt
 import csv
 
-shift_insts = [{'name': 'Development',
-                'label': 'Dv',
-                'start': '0830',
-                'end': '1700'},
-               {'name': 'Early Morning',
-                'label': 'M',
-                'start': '0515',
-                'end': '1400'},
-               {'name': 'Afternoon',
-                'label': 'E',
-                'start': '1200',
-                'end': '1800'},
-               {'name': 'Long Met-shift Day',
-                'label': 'D',
-                'start': '0515',
-                'end': '1700'},
-               {'name': 'Support Shift',
-                'label': 'd\'',
-                'start': '0630',
-                'end': '1600'},
-              ]
 
-
-def create_ical_event(name, start, end, label, day, year='2019', month='12'):
+def create_ical_event(name, start, end, day, label, year='2019', month='12'):
     '''
     creates a ical even with all the key details
     '''
@@ -43,14 +21,16 @@ def create_ical_event(name, start, end, label, day, year='2019', month='12'):
     return msg.format(uid, dtstamp, dtstart, dtend, summary)
 
 
-def create_ical(calendar, shift_types):
+def create_ical(calendar, shift_proto):
     '''
     creates a large string which acts as the ical file
     '''
-    outstring = """BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n"""
+    outstring = ("""BEGIN:VCALENDAR\r\nVERSION:2.0
+                 \r\nPRODID:-//hacksw/handcal//
+                 NONSGML v1.0//EN\r\n""")
     for date, shift in calendar['diary'].iteritems():
 
-        for shift_type in shift_types:
+        for shift_type in shift_proto:
             shift_type['day'] = date
             if shift_type['label'] == shift:
                 shift_type['year'] = calendar['year']
@@ -90,9 +70,9 @@ def get_shift_data(fpath):
         shifts = []
         for row in reader:
             shifts.append({'name': row[0],
-                          'label': row[1],
-                          'start': row[2],
-                          'end': row[3]})
+                           'label': row[1],
+                           'start': row[2],
+                           'end': row[3]})
     return shifts
 
 
@@ -109,10 +89,10 @@ def main():
     '''
     shift_pattern_fp = 'shifts.txt'
     fpath = 'input.csv'
-    shift_insts = get_shift_data(shift_pattern_fp)
+    shift_proto = get_shift_data(shift_pattern_fp)
     calendar = get_csv_data(fpath)
 
-    outstring = create_ical(calendar, shift_insts)
+    outstring = create_ical(calendar, shift_proto)
     fhandle = open('calendar.ics', 'w')
     fhandle.write(outstring)
 
